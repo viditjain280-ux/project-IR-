3b396cc3c02af3c38f2a5ecce3d73b735e8f261b
viditjain280-ux <viditjain280@gmail.com>
2026-07-16 14:17:00 +0530

    initial commit

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\hp\Documents\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\hp\Downloads\pypdf\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
